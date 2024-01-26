709f4b3e952a9ae96a3dec66de21b6a04061b77f
Rajdeep singh <32416397+officialrajdeepsingh@users.noreply.github.com>
2024-01-26 11:50:17 +0000

    update the file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,9 @@ import { ThemeChanger } from "@/components/theme-toggle";
 export default function Home() {
   return (
     <main className="flex min-h-screen-xl flex-col items-center justify-between p-14">
+      <ThemeChanger />
 
-       <ThemeChanger />
-      
-       {/* <Tweets /> */}
+      {/* <Tweets /> */}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         <Tweet id="1718296988729356404" />
@@ -17,7 +16,6 @@ export default function Home() {
         <Tweet id="1633770889119506432" />
         <Tweet id="1581469371322445825" />
       </div>
-
     </main>
   );
 }
